ec2f41ba70794b564d86f0b043b2c8a0ca1b536d
dahlbergba <dahlberg.ba@gmail.com>
2021-02-25 17:05:54 -0500

    Hello World
    It lives! The way this is set up right now, fitness is related not to the correct timing of the agent's first action, nor the first action at all - instead, more actions is more fitness. We expected this to be trivial to evolve, and indeed it was (Fig A).

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #TASK PARAMETERS ---------------------------------------------------------------
-Duration = 1000    # Duration of simulation, in steps
+Duration = 100    # Duration of simulation, in steps
 Dt = 0.01
-N_task_agents = 2 # Number of copies of agent for the time task
+#N_task_agents = 2 # Number of copies of agent for the time task
+Stimuli = [0.1, 0.2]    # External input/stimuli for the time task
 
 #CTRNN PARAMETERS --------------------------------------------------------------
-Size = 10
+Size = 5
 WeightRange = 15
 BiasRange = 15
 TimeConstMin = Dt*10
@@ -18,28 +19,45 @@ TimeConstMax = 5.0
 InputWeightRange = 15
 
 #EA PARAMETERS -----------------------------------------------------------------
-Trials = 10    # N of trials of self-play
+Trials = 5   # N of trials of self-play
 GenotypeLength = Size*Size + Size*3    # Slightly longer because of incoding the input weight vector
-Population = 50
+Population = 20
 RecombProb = 0.5
 MutatProb = 0.1
 Generations = 200
 Tournaments = Generations * Population
 
-# ==============================================================================
+def fitnessFunction(genotype):
+    """This version of the fitness function is meant to be scale-agnostic; the agent can respond in
+    whatever time-scale it wants so long as it is within the trial Duration. The agent's fitness is 
+    based on the ratio consistency of its response times."""
+    
+    agent = Agent(genotype, Size, WeightRange, BiasRange, TimeConstMin, TimeConstMax, InputWeightRange, Dt)
+    first_actions = np.empty((len(Stimuli), Trials))
+    for s in range(len(Stimuli)):    # This loops runs each stimulus condition
+            for t in range(Trials):    # This loops run all trials of task
+                actions = np.empty(Duration)
+                agent.sense(Stimuli[s])    # Initial stimulus
+                for step in range(Duration):    # Runtime
+                    agent.think()
+                    actions[step] = agent.act()    # Record agent action
+                    agent.sense(0)    # Under current experimental design, no stimulus available for rest of 
+                    
+    #            first_actions[s, t] = np.where((actions==1))[0][0]    # Record when the agent's first action was   
+    #Calculate ratio of stimuli to each other
+    #Calculate ration of first action/response time to each other
+    #Pass these through some loss function, returning fitness                
+                    
+                first_actions[s, t] = np.count_nonzero(actions)  # TEMP
+    return np.average(first_actions)/Duration # TEMP
 
-def fitness_function(genotype):
 
-    agents = []
-    for i in range(len(agents)): 
-        agents[i] = Agent(Size)
-        agents[i].setParameters(genotype,WeightRange,BiasRange,TimeConstMin,TimeConstMax,InputWeightRange)
-    nn = CTRNN(Size)
+# ============================= RUNTIME ========================================
+    
+
+test = Microbial(fitnessFunction, Population, GenotypeLength, RecombProb, MutatProb)
+test.run(Tournaments)
+test.showFitness()
 
 
 
-    fitness = np.zeros(Trials)
-    for t in range(Trials):    # This loops run all trials of task
-        pass
-
-    return np.average(fitness)
